ca05ea5083a5f4a0b522c798b90254ab0194822c
Muhammad Najah Yuga Desyalwa <najahyugad@gmail.com>
2024-05-19 20:54:27 +0700

    Feat: Add new feature login and update routes/web.php

--- routes/web.php
+++ routes/web.php
@@ -10,16 +10,26 @@ use App\Http\Controllers\TeachersController;
 use App\Http\Controllers\UsersController;
 use Illuminate\Support\Facades\Route;
 
+// akses tanpa harus login
+Route::middleware(['guest'])->group(function () {
+    Route::get('/', [AuthController::class, 'dashboard']);
+    Route::get('/login', [AuthController::class, 'indexLogin']);
+    Route::post('/login', [AuthController::class, 'login'])->name('login');
+});
 
-Route::get('/', [AuthController::class, 'dashboard']);
-Route::get('/admin', [AuthController::class, 'indexAdmin']);
+Route::middleware(['auth'])->group(function () {
+    Route::get('/admin', [AuthController::class, 'indexAdmin'])->middleware('UsersAkses:admin'); // pembatasan hak akses hanya untuk admin
+    Route::get('/guru', [AuthController::class, 'indexGuru'])->middleware('UsersAkses:guru'); // pembatasan hak akses hanya untuk guru
+    Route::get('logout', [AuthController::class, 'logout'])->name('logout');
 
-Route::resource('/educationLevels', EducationLevelsController::class);
-Route::resource('/teacher', TeachersController::class);
-Route::resource('/semesters', App\Http\Controllers\SemestersController::class);
-Route::resource('/classrooms', App\Http\Controllers\ClassroomsController::class);
-Route::resource('/students', StudentsController::class);
-Route::resource('/users', UsersController::class);
-Route::resource('/categoryCourses', CategoryCoursesController::class);
-Route::resource('/courses', CoursesController::class);
-Route::resource('/tasks', TasksController::class);
+    Route::resource('/educationLevels', EducationLevelsController::class);
+    Route::resource('/teacher', TeachersController::class);
+    Route::resource('/semesters', App\Http\Controllers\SemestersController::class);
+    Route::resource('/classrooms', App\Http\Controllers\ClassroomsController::class);
+    Route::resource('/students', StudentsController::class);
+    Route::resource('/users', UsersController::class);
+    Route::resource('/categoryCourses', CategoryCoursesController::class);
+    Route::resource('/courses', CoursesController::class);
+    Route::resource('/tasks', TasksController::class);
+});
+
