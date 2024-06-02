74da026e6ea2778d826a05525400a419d2ed8283
Muhammad Najah Yuga Desyalwa <najahyugad@gmail.com>
2024-06-02 23:54:13 +0700

    Added seeder and update factory in users

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -2,6 +2,7 @@
 
 namespace Database\Factories;
 
+use App\Models\User;
 use Illuminate\Database\Eloquent\Factories\Factory;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Str;
@@ -11,6 +12,7 @@ use Illuminate\Support\Str;
  */
 class UserFactory extends Factory
 {
+    protected $model = User::class;
     /**
      * The current password being used by the factory.
      */
@@ -24,11 +26,11 @@ class UserFactory extends Factory
     public function definition(): array
     {
         return [
-            'name' => fake()->name(),
+            'username' => fake()->name(),
             'email' => fake()->unique()->safeEmail(),
-            'email_verified_at' => now(),
-            'password' => static::$password ??= Hash::make('password'),
-            'remember_token' => Str::random(10),
+            // 'email_verified_at' => now(),
+            'password' => static::$password ??= Hash::make('password')
+            // 'remember_token' => Str::random(10),
         ];
     }
 
