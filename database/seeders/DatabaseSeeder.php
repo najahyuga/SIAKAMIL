ed731adf3caee66bed2afda0b2c0698c4c50f58a
Muhammad Najah Yuga Desyalwa <najahyugad@gmail.com>
2024-06-03 17:57:05 +0700

    Added new database seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,7 +2,6 @@
 
 namespace Database\Seeders;
 
-use App\Models\User;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
@@ -13,11 +12,16 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // User::factory(10)->create();
-
-        User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
-        ]);
+        $this->call(RolesSeeder::class);
+        $this->call(UsersSeeder::class);
+        $this->call(UsersRolesSeeder::class);
+        $this->call(FilesUploadsSeeder::class);
+        $this->call(EducationLevelsSeeder::class);
+        $this->call(TeachersSeeder::class);
+        $this->call(SemestersSeeder::class);
+        $this->call(ClassroomsSeeder::class);
+        $this->call(StudentsSeeder::class);
+        $this->call(MasterCategoryCoursesSeeder::class);
+        $this->call(MasterCoursesSeeder::class);
     }
 }
