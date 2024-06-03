0139566eac7dabf2cea00756f91d7dabcc79057d
Muhammad Najah Yuga Desyalwa <najahyugad@gmail.com>
2024-06-03 18:30:22 +0700

    Fix: Fixed add definition relation to users table

--- app/Models/Roles.php
+++ app/Models/Roles.php
@@ -15,6 +15,6 @@ class Roles extends Model
 
     public function users()
     {
-        return $this->belongsToMany(User::class);
+        return $this->belongsToMany(User::class, 'users_roles');
     }
 }
