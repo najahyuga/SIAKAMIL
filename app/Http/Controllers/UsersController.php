f981d65509eba2ea4eabc7daa54d5ea6da6b736a
Muhammad Najah Yuga Desyalwa <najahyugad@gmail.com>
2024-06-04 10:34:21 +0700

    fix: fixed relation code on role table, teacher table, student table improvement in users controller

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Http\Controllers\Controller;
+use App\Models\Roles;
 use App\Models\Teachers;
 use App\Models\User;
 use Illuminate\Http\Request;
@@ -17,7 +18,7 @@ class UsersController extends Controller
     {
         try {
             // get all data to display to index page
-            $users = User::all();
+            $users = User::with('roles')->get();
 
             // mengembalikan ke halaman index
             return view('admin.users.index', compact('users'));
@@ -91,12 +92,16 @@ class UsersController extends Controller
     {
         try {
             // get data to display in show page
-            $user = User::findOrFail($id);
+            $user = User::with(['roles', 'teacher', 'student'])->findOrFail($id);
 
-            $teacher = Teachers::select('id', 'name');
+            $teacher = $user->teacher;
+            // Teachers::with('user')->get();
+
+            // ambil data roles
+            $roles = Roles::all();
 
             // mengembalikan ke halaman show
-            return view('admin.users.show', ['teacher' => $teacher], compact('user'));
+            return view('admin.users.show', ['teacher' => $teacher], compact('user', 'roles'));
         } catch (\Throwable $th) {
             Log::error("Tidak dapat mengambil data ". $th->getMessage());
             response()->json([
@@ -113,10 +118,13 @@ class UsersController extends Controller
     {
         try {
             // get data to display in edit page
-            $user = User::findOrFail($id);
+            $user = User::with('roles')->findOrFail($id);
+
+            // ambil data roles
+            $roles = Roles::all();
 
             // mengembalikan ke halaman edit
-            return view('admin.users.edit', compact(['user']));
+            return view('admin.users.edit', compact(['user', 'roles']));
         } catch (\Throwable $th) {
             Log::error("Tidak dapat mengambil data ". $th->getMessage());
             response()->json([
@@ -134,34 +142,32 @@ class UsersController extends Controller
     public function update(Request $request, string $id)
     {
         try {
-            // validate form
+            // Validasi form
             $request->validate([
-                'username'                  => 'required|min:4',
-                'email'                     => 'required|min:5|email',
-                'password'                  => 'required|min:6',
-                'level'                     => 'required'
+                'username' => 'required|min:4',
+                'email' => 'required|min:5|email',
+                'password' => 'required|min:6'
             ]);
 
-            // update data user
+            // Update data user
             $user = User::findOrFail($id);
             $user->update([
-                'username'      => $request->username,
-                'email'         => $request->email,
-                'password'      => $request->password,
-                'level'         => $request->level
+                'username' => $request->username,
+                'email' => $request->email,
+                'password' => $request->password
             ]);
 
-            // mengembalikan ke halaman index
+            // Update roles user
+            $user->roles()->sync($request->roles); // Di sini, $request->roles adalah array dari ID roles yang dipilih dari form.
+
+            // Mengembalikan ke halaman index
             return redirect()->route('admin.users.index')->with(['success' => 'Data Berhasil Diubah!']);
         } catch (\Throwable $th) {
-            Log::error("Tidak dapat mengubah data ". $th->getMessage());
-            response()->json([
-                'status'    => false,
-                'message'   => 'Tidak dapat mengubah data'
-            ], 500);
+            Log::error("Tidak dapat mengubah data " . $th->getMessage());
+            return redirect()->back()->with(['error' => 'Tidak dapat mengubah data']);
         }
     }
-
+    
     /**
      * Remove the specified resource from storage.
      */
