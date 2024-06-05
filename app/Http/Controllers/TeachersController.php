8771a324fa4b648accd924f8d658c2539da00f61
Muhammad Najah Yuga Desyalwa <najahyugad@gmail.com>
2024-06-05 21:06:33 +0700

    Fix: Fixed bug crud code and relation in teachers controller

--- app/Http/Controllers/TeachersController.php
+++ app/Http/Controllers/TeachersController.php
@@ -4,11 +4,14 @@ namespace App\Http\Controllers;
 
 use App\Http\Controllers\Controller;
 use App\Models\EducationLevels;
+use App\Models\FilesUploads;
+use App\Models\Roles;
 use App\Models\Teachers;
 use App\Models\User;
 use Illuminate\Contracts\View\View;
 use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Log;
 use Illuminate\Support\Facades\Storage;
 
 class TeachersController extends Controller
@@ -18,9 +21,17 @@ class TeachersController extends Controller
      */
     public function index()
     {
-        //get data to view index
-        $teachers = Teachers::all();
-        return view('admin.teachers.index', compact('teachers'));
+        try {
+            //get data to view index
+            $teachers = Teachers::all();
+            return view('admin.teachers.index', compact('teachers'));
+        } catch (\Throwable $th) {
+            Log::error("Tidak dapat mengambil data ". $th->getMessage());
+            response()->json([
+                'status'    => false,
+                'message'   => 'Tidak dapat mengambil data'
+            ], 500);
+        }
     }
 
     /**
@@ -28,8 +39,17 @@ class TeachersController extends Controller
      */
     public function create()
     {
-        $education_levels_id = EducationLevels::select('id', 'name')->get();
-        return view('admin.teachers.create', ['education_levels_id' => $education_levels_id]);
+        try {
+            $education_levels_id = EducationLevels::select('id', 'name')->get();
+            $roles = Roles::all();
+            return view('admin.teachers.create', ['education_levels_id' => $education_levels_id, 'roles' => $roles]);
+        } catch (\Throwable $th) {
+            Log::error("Tidak dapat menampilkan halaman ". $th->getMessage());
+            response()->json([
+                'status'    => false,
+                'message'   => 'Tidak dapat menampilkan halaman'
+            ], 500);
+        }
     }
 
     /**
@@ -37,48 +57,83 @@ class TeachersController extends Controller
      */
     public function store(Request $request): RedirectResponse
     {
-        //validate form
-        $request->validate([
-            'name'                      => 'required|min:4',
-            'address'                   => 'required|min:10',
-            'gender'                    => 'required',
-            'dateOfBirth'               => 'required',
-            'image'                     => 'required|image|mimes:jpeg,jpg,png|max:2048',
-            'status'                    => 'required',
-            'education_levels_id'       => 'required',
+        try {
+            //validate form
+            $request->validate([
+                'name'                      => 'required|min:4',
+                'address'                   => 'required|min:10',
+                'gender'                    => 'required',
+                'dateOfBirth'               => 'required',
+                'status'                    => 'required',
+                'education_levels_id'       => 'required|exists:education_levels,id',
+                // 'users_id'                  => 'required|exists:users,id',
+                'files_uploads_id'          => 'exists:files_uploads,id',
 
-            'username'                  => 'required|min:4',
-            'email'                     => 'required|min:5|email',
-            'password'                  => 'required|min:6',
-            'level'                     => 'required'
-        ]);
+                // table users
+                'username'                  => 'required|min:4',
+                'email'                     => 'required|min:5|email',
+                'password'                  => 'required|min:6',
 
-        //upload image
-        $image = $request->file('image');
-        $image->storeAs('public/images', $image->hashName());
+                // table roles
+                'level.*'                   => 'exists:roles,id',
 
-        //create data teacher
-        $teachers = Teachers::create([
-            'name'                      => $request->name,
-            'address'                   => $request->address,
-            'gender'                    => $request->gender,
-            'dateOfBirth'               => $request->dateOfBirth,
-            'image'                     => $image->hashName(),
-            'status'                    => $request->status,
-            'education_levels_id'       => $request->education_levels_id,
-        ]);
+                // table files_uploads
+                'path'                      => 'required|image|mimes:jpeg,jpg,png|max:2048',
+            ]);
 
-        // create data user
-        User::create([
-            'username'      => $request->username,
-            'email'         => $request->email,
-            'password'      => $request->password,
-            'level'         => $request->level,
-            'teachers_id'   => $teachers->id
-        ]);
+            // Upload new image
+            $path = $request->file('path');
+            $path->getClientOriginalName();
+            $imagePath = $path->storeAs('public/images', $path->getClientOriginalName());
 
-        //redirect to index
-        return redirect()->route('admin.teacher.index')->with(['success' => 'Data Berhasil Disimpan!']);
+            // create data user
+            $user = User::create([
+                'username'      => $request->username,
+                'email'         => $request->email,
+                'password'      => $request->password,
+            ]);
+
+            // Tangani array nilai level
+            if ($request->has('level')) {
+                // Tetapkan nilai level yang dipilih ke atribut roles
+                $user->roles()->sync($request->input('level'));
+            } else {
+                // Tetapkan nilai default ke atribut roles jika tidak ada level yang dipilih
+                // Misalnya, default level sebagai calonSiswa
+                $defaultRole = Roles::where('level', 'calonSiswa')->first();
+                if ($defaultRole) {
+                    $user->roles()->sync([$defaultRole->id]);
+                }
+            }
+
+            // if ($request->has('level')) {
+            //     $user->roles()->sync($request->input('level'));
+            // }
+
+            // create data files_uploads record
+            $file_uploads = FilesUploads::updateOrCreate(
+                ['id' => $request->files_uploads_id],
+                ['path' => basename($imagePath)]
+            );
+
+            //create data teacher
+            Teachers::create([
+                'name'                      => $request->name,
+                'address'                   => $request->address,
+                'gender'                    => $request->gender,
+                'dateOfBirth'               => $request->dateOfBirth,
+                'status'                    => $request->status,
+                'education_levels_id'       => $request->education_levels_id,
+                'users_id'                  => $user->id,
+                'files_uploads_id'          => $file_uploads->id
+            ]);
+
+            //redirect to index
+            return redirect()->route('admin.teacher.index')->with(['success' => 'Data Berhasil Disimpan!']);
+        } catch (\Throwable $th) {
+            Log::error("Tidak dapat menyimpan data: " . $th->getMessage());
+            return redirect()->back()->with(['error' => 'Tidak dapat menyimpan data'])->withInput();
+        }
     }
 
     /**
@@ -86,18 +141,25 @@ class TeachersController extends Controller
      */
     public function show(string $id)
     {
-        // get data based on id and name
-        $education_levels_id = EducationLevels::select('id', 'name')->get();
+        try {
+            // display data based on ID
+            // menampilkan data berdasarkan ID
+            $teacher = Teachers::with(['education_levels', 'user'])->findOrFail($id);
 
-        // get data based on id and level
-        $user = User::select('id', 'level')->get();
+            // get data relation
+            $education_levels_id = EducationLevels::where('id', '!=', $teacher->education_levels_id)->get();
+            $user = User::where('id', '!=', $teacher->user)->get();
+            $roles = Roles::all();
 
-        // display data based on ID
-        // menampilkan data berdasarkan ID
-        $teacher = Teachers::findOrFail($id);
-
-        // mengembalikan ke halaman show
-        return view('admin.teachers.show', ['education_levels_id' => $education_levels_id, 'user' => $user], compact(['teacher']));
+            // mengembalikan ke halaman show
+            return view('admin.teachers.show', compact('teacher', 'education_levels_id', 'user', 'roles'));
+        } catch (\Throwable $th) {
+            Log::error("Tidak dapat mengambil data ". $th->getMessage());
+            response()->json([
+                'status'    => false,
+                'message'   => 'Tidak dapat mengambil data'
+            ], 500);
+        }
     }
 
     /**
@@ -105,20 +167,17 @@ class TeachersController extends Controller
      */
     public function edit(string $id)
     {
-        // get data based on id and name
-        // menampilkan data berdasarkan ID dan nama jenjang pendidikan
-        $education_levels_id = EducationLevels::select('id', 'name')->get();
-
-        // get data based on id and level
-        // menampilkan data berdasarkan ID dan level
-        $user = User::select('id', 'level')->get();
-
         // display data based on ID
         // menampilkan data berdasarkan ID
-        $teacher = Teachers::findOrFail($id);
+        $teacher = Teachers::with(['education_levels', 'user', 'files_uploads'])->findOrFail($id);
+
+        // get data relation
+        $education_levels_id = EducationLevels::where('id', '!=', $teacher->education_levels_id)->get();
+        $user = User::where('id', '!=', $teacher->user)->get();
+        $roles = Roles::all();
 
         // mengembalikan ke halaman edit
-        return view('admin.teachers.edit', ['education_levels_id' => $education_levels_id], compact(['teacher', 'user']));
+        return view('admin.teachers.edit',compact('teacher', 'education_levels_id', 'user', 'roles'));
     }
 
     /**
@@ -126,69 +185,87 @@ class TeachersController extends Controller
      */
     public function update(Request $request, $id): RedirectResponse
     {
-        //validate form
-        $request->validate([
-            'name'                      => 'required|min:4',
-            'address'                   => 'required|min:10',
-            'gender'                    => 'required',
-            'dateOfBirth'               => 'required',
-            'image'         => 'image|mimes:jpeg,jpg,png|max:2048',
-            'status'                    => 'required',
-            'education_levels_id'       => 'required',
+        try {
+            //validate form
+            $request->validate([
+                'name'                      => 'required|min:4',
+                'address'                   => 'required|min:10',
+                'gender'                    => 'required',
+                'dateOfBirth'               => 'required',
+                'status'                    => 'required',
+                'education_levels_id'       => 'required|exists:education_levels,id',
 
-            'username'                  => 'required|min:4',
-            'email'                     => 'required|min:5|email',
-            'password'                  => 'required|min:6',
-            'level'                     => 'required'
-        ]);
+                // table users
+                'username'                  => 'required|min:4',
+                'email'                     => 'required|min:5|email',
+                'password'                  => 'required|min:6',
 
-        // get data by id
-        $teacher = Teachers::findOrFail($id);
+                // table roles
+                'level.*'                   => 'exists:roles,id',
 
-        // cek image apakah diupload
-        if ($request->hasFile('image')) {
-            // upload image baru
-            $image = $request->file('image');
-            $image->storeAs('public/images', $image->hashName());
+                'path'                      => 'image|mimes:jpeg,jpg,png|max:2048',
+                // table files_uploads
+            ]);
 
-            // delete image lama
-            Storage::delete('public/images/'.$teacher->image);
+            // Get data by ID
+            $teacher = Teachers::with('user', 'files_uploads')->findOrFail($id);
 
-            // update data dengan image baru
+            // Update or create user data
+            $user = User::updateOrCreate(
+                ['id' => $teacher->users_id], // Add id here
+                [
+                    'username' => $request->username,
+                    'email' => $request->email,
+                    'password' => $request->password
+                ]
+            );
+            $user->save();
+
+            // Tangani array nilai level
+            if ($request->has('level')) {
+                $user->roles()->sync($request->input('level'));
+            }
+            // Update or create files_uploads data
+            if ($request->hasFile('path')) {
+
+                // Upload new image
+                $path = $request->file('path');
+                $path->getClientOriginalName();
+                $imagePath = $path->storeAs('public/images', $path->getClientOriginalName());
+
+                // Delete old image if exists
+                if ($teacher->files_uploads && $teacher->files_uploads->path) {
+                    Storage::delete('public/images/' . $teacher->files_uploads->path);
+                }
+
+                $filesUpload = FilesUploads::updateOrCreate(
+                    ['id' => $teacher->files_uploads_id],
+                    ['path' => basename($imagePath)]
+                );
+
+                $teacher->files_uploads_id = $filesUpload->id;
+            }
+
+            // Update teacher data
             $teacher->update([
-                'name'                      => $request->name,
-                'address'                   => $request->address,
-                'gender'                    => $request->gender,
-                'dateOfBirth'               => $request->dateOfBirth,
-                'image'                     => $image->hashName(),
-                'status'                    => $request->status,
-                'education_levels_id'       => $request->education_levels_id
+                'name' => $request->name,
+                'address' => $request->address,
+                'gender' => $request->gender,
+                'dateOfBirth' => $request->dateOfBirth,
+                'status' => $request->status,
+                'education_levels_id' => $request->education_levels_id,
+                'users_id' => $user->id,
             ]);
-        } else {
-            // update data tanpa image
-            $teacher->update([
-                'name'                      => $request->name,
-                'address'                   => $request->address,
-                'gender'                    => $request->gender,
-                'dateOfBirth'               => $request->dateOfBirth,
-                'status'                    => $request->status,
-                'education_levels_id'       => $request->education_levels_id
-            ]);
+
+            // Save teacher
+            $teacher->save();
+
+            // redirect to index
+            return redirect()->route('admin.teacher.index')->with(['success' => 'Data Berhasil Diubah!']);
+        } catch (\Throwable $th) {
+            Log::error("Tidak dapat mengubah data teacher {$id}: " . $th->getMessage());
+            return redirect()->back()->with(['error' => 'Tidak dapat mengupdate data'])->withInput();
         }
-        // update table user
-        if ($teacher->save()) {
-            $user = User::findOrFail($id);
-            $user->update([
-                'username'      => $request->username,
-                'email'         => $request->email,
-                'password'      => $request->password,
-                'level'         => $request->level,
-                'teachers_id'   => $teacher->id
-            ]);
-        }
-        $user->save();
-        // redirect to index
-        return redirect()->route('admin.teacher.index')->with(['success' => 'Data Berhasil Diubah!']);
     }
 
     /**
