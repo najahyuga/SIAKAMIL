02aa515a6de29c2228a490ca7ef2758680f4e2f1
Muhammad Najah Yuga Desyalwa <najahyugad@gmail.com>
2024-07-12 21:17:26 +0700

    Feat: Added new relation to formsiswa

--- app/Models/User.php
+++ app/Models/User.php
@@ -65,4 +65,9 @@ class User extends Authenticatable
     {
         return $this->roles()->where('level', $level)->exists();
     }
+
+    public function formSiswa()
+    {
+        return $this->hasOne(FormSiswa::class, 'users_id');
+    }
 }
