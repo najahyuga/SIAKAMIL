4fa7b1b9f2f70a333574bb770960f68656b43c09
ericwahyu <erickwahyu19@gmail.com>
2024-07-13 17:55:02 +0000

    Fix Update Eye Password

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -10,24 +10,29 @@
     <meta content="" name="keywords">
 
     <!-- Favicons -->
-    <link href="{{asset('backend/assets/img/logopkbm.jpeg')}}" rel="icon">
-    <link href="{{asset('backend/assets/img/apple-touch-icon.png')}}" rel="apple-touch-icon">
+    <link href="{{ asset('backend/assets/img/logopkbm.jpeg') }}" rel="icon">
+    <link href="{{ asset('backend/assets/img/apple-touch-icon.png') }}" rel="apple-touch-icon">
 
     <!-- Google Fonts -->
     <link href="https://fonts.gstatic.com" rel="preconnect">
     <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i|Nunito:300,300i,400,400i,600,600i,700,700i|Poppins:300,300i,400,400i,500,500i,600,600i,700,700i" rel="stylesheet">
 
     <!-- Vendor CSS Files -->
-    <link href="{{asset('backend/assets/vendor/bootstrap/css/bootstrap.min.css')}}" rel="stylesheet">
-    <link href="{{asset('backend/assets/vendor/bootstrap-icons/bootstrap-icons.css')}}" rel="stylesheet">
-    <link href="{{asset('backend/assets/vendor/boxicons/css/boxicons.min.css')}}" rel="stylesheet">
-    <link href="{{asset('backend//vendor/quill/quill.snow.css')}}" rel="stylesheet">
-    <link href="{{asset('backend/assets/vendor/quill/quill.bubble.css')}}" rel="stylesheet">
-    <link href="{{asset('backend/assets/vendor/remixicon/remixicon.css')}}" rel="stylesheet">
-    <link href="{{asset('backend/assets/vendor/simple-datatables/style.css')}}" rel="stylesheet">
-
+    <link href="{{ asset('backend/assets/vendor/bootstrap/css/bootstrap.min.css') }}" rel="stylesheet">
+    <link href="{{ asset('backend/assets/vendor/bootstrap-icons/bootstrap-icons.css') }}" rel="stylesheet">
+    <link href="{{ asset('backend/assets/vendor/boxicons/css/boxicons.min.css') }}" rel="stylesheet">
+    <link href="{{ asset('backend//vendor/quill/quill.snow.css') }}" rel="stylesheet">
+    <link href="{{ asset('backend/assets/vendor/quill/quill.bubble.css') }}" rel="stylesheet">
+    <link href="{{ asset('backend/assets/vendor/remixicon/remixicon.css') }}" rel="stylesheet">
+    <link href="{{ asset('backend/assets/vendor/simple-datatables/style.css') }}" rel="stylesheet">
+    <script src="//unpkg.com/alpinejs" defer></script>
     <!-- Template Main CSS File -->
-    <link href="{{asset('backend/assets/css/style.css')}}" rel="stylesheet">
+    <link href="{{ asset('backend/assets/css/style.css') }}" rel="stylesheet">
+    <style>
+        .cursor-pointer{
+            cursor: pointer;
+        }
+    </style>
 </head>
 
 <body>
@@ -39,8 +44,8 @@
                         <div class="col-lg-4 col-md-6 d-flex flex-column align-items-center justify-content-center">
                             <div class="d-flex justify-content-center py-4">
                                 <a href="/" class="logo d-flex align-items-center w-auto">
-                                <img src="{{asset('backend/assets/img/logopkbm.jpeg')}}" alt="SIAKAMIL">
-                                <span class="d-none d-lg-block">SIAKAMIL</span>
+                                    <img src="{{ asset('backend/assets/img/logopkbm.jpeg') }}" alt="SIAKAMIL">
+                                    <span class="d-none d-lg-block">SIAKAMIL</span>
                                 </a>
                             </div><!-- End Logo -->
 
@@ -55,7 +60,7 @@
                                         {{-- @if ($errors->any())
                                             <div class="alert alert-danger">
                                                 {{-- Data Users belum Tersedia. --}}
-                                                {{-- <ul>
+                                        {{-- <ul>
                                                     @foreach ($errors->all() as $item)
                                                         <li>{{ $item }}</li>
                                                     @endforeach
@@ -66,7 +71,7 @@
                                         <div class="col-12">
                                             <label for="yourUsername" class="form-label">Username</label>
                                             <div class="input-group has-validation">
-                                                <input type="text" value="{{ old('username') }}" name="username" class="form-control @error('username') is-invalid @enderror" >
+                                                <input type="text" value="{{ old('username') }}" name="username" class="form-control @error('username') is-invalid @enderror">
                                                 <div class="invalid-feedback">Please enter your username.</div>
                                             </div>
                                             <!-- error message untuk username -->
@@ -77,9 +82,12 @@
                                             @enderror
                                         </div>
 
-                                        <div class="col-12">
+                                        <div class="col-12" x-data="{ openPassword: '' }">
                                             <label for="yourPassword" class="form-label">Password</label>
-                                            <input type="password" name="password" class="form-control @error('password') is-invalid @enderror" >{{--required--}}
+                                            <div class="input-group">
+                                                <input :type="openPassword ? 'text' : 'password'" name="password" class="form-control @error('password') is-invalid @enderror">
+                                                <div class="input-group-text cursor-pointer" @click="openPassword =! openPassword"><i :class="openPassword ? 'ri-eye-line' : 'ri-eye-off-line'"></i></div>
+                                            </div>{{-- required --}}
                                             <div class="invalid-feedback">Please enter your password!</div>
                                             <!-- error message untuk password -->
                                             @error('password')
@@ -118,17 +126,17 @@
     <a href="#" class="back-to-top d-flex align-items-center justify-content-center"><i class="bi bi-arrow-up-short"></i></a>
 
     <!-- Vendor JS Files -->
-    <script src="{{asset('backend/assets/vendor/apexcharts/apexcharts.min.js')}}"></script>
-    <script src="{{asset('backend/assets/vendor/bootstrap/js/bootstrap.bundle.min.js')}}"></script>
-    <script src="{{asset('backend/assets/vendor/chart.js/chart.umd.js')}}"></script>
-    <script src="{{asset('backend/assets/vendor/echarts/echarts.min.js')}}"></script>
-    <script src="{{asset('backend/assets/vendor/quill/quill.min.js')}}"></script>
-    <script src="{{asset('backend/assets/vendor/simple-datatables/simple-datatables.js')}}"></script>
-    <script src="{{asset('backend/assets/vendor/tinymce/tinymce.min.js')}}"></script>
-    <script src="{{asset('backend/assets/vendor/php-email-form/validate.js')}}"></script>
+    <script src="{{ asset('backend/assets/vendor/apexcharts/apexcharts.min.js') }}"></script>
+    <script src="{{ asset('backend/assets/vendor/bootstrap/js/bootstrap.bundle.min.js') }}"></script>
+    <script src="{{ asset('backend/assets/vendor/chart.js/chart.umd.js') }}"></script>
+    <script src="{{ asset('backend/assets/vendor/echarts/echarts.min.js') }}"></script>
+    <script src="{{ asset('backend/assets/vendor/quill/quill.min.js') }}"></script>
+    <script src="{{ asset('backend/assets/vendor/simple-datatables/simple-datatables.js') }}"></script>
+    <script src="{{ asset('backend/assets/vendor/tinymce/tinymce.min.js') }}"></script>
+    <script src="{{ asset('backend/assets/vendor/php-email-form/validate.js') }}"></script>
 
     <!-- Template Main JS File -->
-    <script src="{{asset('backend/assets/js/main.js')}}"></script>
+    <script src="{{ asset('backend/assets/js/main.js') }}"></script>
 
     <!-- library sweetalert -->
     <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
@@ -136,7 +144,7 @@
     <!-- session success &  error -->
     <script>
         //message with sweetalert
-        @if(session('success'))
+        @if (session('success'))
             Swal.fire({
                 icon: "success",
                 title: "BERHASIL",
@@ -144,7 +152,7 @@
                 showConfirmButton: false,
                 timer: 2000
             });
-        @elseif(session('error'))
+        @elseif (session('error'))
             Swal.fire({
                 icon: "error",
                 title: "GAGAL!",
@@ -156,4 +164,5 @@
     </script>
 
 </body>
+
 </html>
