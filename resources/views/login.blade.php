415656af58eab1082e0c06254307a73313682fce
Muhammad Najah Yuga Desyalwa <najahyugad@gmail.com>
2024-07-24 19:20:34 +0700

    Fix: Fixed bug code in login

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -20,7 +20,7 @@
     <link href="{{ asset('backend/assets/vendor/bootstrap/css/bootstrap.min.css') }}" rel="stylesheet">
     <link href="{{ asset('backend/assets/vendor/bootstrap-icons/bootstrap-icons.css') }}" rel="stylesheet">
     <link href="{{ asset('backend/assets/vendor/boxicons/css/boxicons.min.css') }}" rel="stylesheet">
-    <link href="{{ asset('backend/vendor/quill/quill.snow.css') }}" rel="stylesheet">
+    <link href="{{ asset('backend/assets/vendor/quill/quill.snow.css') }}" rel="stylesheet">
     <link href="{{ asset('backend/assets/vendor/quill/quill.bubble.css') }}" rel="stylesheet">
     <link href="{{ asset('backend/assets/vendor/remixicon/remixicon.css') }}" rel="stylesheet">
     <link href="{{ asset('backend/assets/vendor/simple-datatables/style.css') }}" rel="stylesheet">
